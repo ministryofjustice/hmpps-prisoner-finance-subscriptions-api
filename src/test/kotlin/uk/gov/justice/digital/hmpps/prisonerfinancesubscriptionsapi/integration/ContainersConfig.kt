aa0923772020be225c5e7b8d24b5026a1185bfdf
tristan-madetech <tristan.moss@justice.gov.uk>
2026-09-15 15:31:43 +0100

    upgrade psql (#14)
    * upgrade psql
    
    * udate psql to 16.15
    
    ---------
    
    Co-authored-by: emilyfbgov <emily.bennett@justice.gov.uk>

--- src/test/kotlin/uk/gov/justice/digital/hmpps/prisonerfinancesubscriptionsapi/integration/ContainersConfig.kt
+++ src/test/kotlin/uk/gov/justice/digital/hmpps/prisonerfinancesubscriptionsapi/integration/ContainersConfig.kt
@@ -10,5 +10,5 @@ class ContainersConfig {
 
   @Bean
   @ServiceConnection
-  fun postgres(): PostgreSQLContainer<*> = PostgreSQLContainer("postgres:16")
+  fun postgres(): PostgreSQLContainer<*> = PostgreSQLContainer("postgres:16.15")
 }
